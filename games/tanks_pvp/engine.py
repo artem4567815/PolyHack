6304af5015ff922b619917da8e73f78f6438b01c
I <artemiy_08@icloud.om>
2024-01-20 16:11:52 +0300

    newCom

--- games/tanks_pvp/engine.py
+++ games/tanks_pvp/engine.py
@@ -3,43 +3,104 @@ import random
 import time
 
 def make_choice():
-    actions = ["up", "down",
-               "left", "right", "fire_up", "fire_down", "fire_left", "fire_right"]
+    actions = ["up", "down", "left", "right", "fire_up", "fire_down", "fire_right", "fire_left"]
     return random.choice(actions)
 
-def MoveTanks(result1, game_map, pos_player1):
+def MoveTanks(result1, result2, game_map, pos_player1, player1Way, pos_player2, player2Way):
     if result1 == 'left' and game_map[pos_player1[0]][pos_player1[1] - 1] not in [1, 3]:
         pos_player1[1] = pos_player1[1] - 1
+        player1Way["way"] = "left"
     elif result1 == 'right' and game_map[pos_player1[0]][pos_player1[1] + 1] not in [1, 3]:
         pos_player1[1] = pos_player1[1] + 1
+        player1Way["way"] = "right"
     elif result1 == 'down' and game_map[pos_player1[0] + 1][pos_player1[1]] not in [1, 3]:
         pos_player1[0] = pos_player1[0] + 1
+        player1Way["way"] = "down"
     elif result1 == 'up' and game_map[pos_player1[0] - 1][pos_player1[1]] not in [1, 3]:
         pos_player1[0] = pos_player1[0] - 1
+        player1Way["way"] = "up"
 
-def Fire(game_map, result1, pos_player1, player1Xp, player1Bullet, pos_player2, player2Xp):
+    if result2 == 'left' and game_map[pos_player2[0]][pos_player2[1] - 1] not in [1, 3]:
+        pos_player2[1] = pos_player2[1] - 1
+        player2Way["way"] = "left"
+    elif result2 == 'right' and game_map[pos_player2[0]][pos_player2[1] + 1] not in [1, 3]:
+        pos_player2[1] = pos_player2[1] + 1
+        player2Way["way"] = "right"
+    elif result2 == 'down' and game_map[pos_player2[0] + 1][pos_player2[1]] not in [1, 3]:
+        pos_player2[0] = pos_player2[0] + 1
+        player2Way["way"] = "down"
+    elif result2 == 'up' and game_map[pos_player2[0] - 1][pos_player2[1]] not in [1, 3]:
+        pos_player2[0] = pos_player2[0] - 1
+        player2Way["way"] = "up"
+
+def Fire(game_map, result1, result2, pos_player1, player1Xp, player1Bullet, pos_player2, player2Xp, player2Bullet):
     if result1 == 'fire_up':
-        player1Bullet["pos"] = [pos_player1[0], pos_player1[0] - 1]
+        player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
         player1Bullet["way"] = "up"
     elif result1 == 'fire_down':
-        player1Bullet["pos"] = [pos_player1[0], pos_player1[0] + 1]
+        player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
         player1Bullet["way"] = "down"
     elif result1 == 'fire_right':
-        player1Bullet["pos"] = [pos_player1[1] + 1, pos_player1[1]]
+        player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
         player1Bullet["way"] = "right"
     elif result1 == 'fire_left':
-        player1Bullet["pos"] = [pos_player1[1] - 1, pos_player1[1]]
+        player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
         player1Bullet["way"] = "left"
 
-def Bullet_up(player1Bullet):
+    if result2 == 'fire_up':
+        player2Bullet["pos"] = [pos_player2[0], pos_player2[1]]
+        player2Bullet["way"] = "up"
+    elif result2 == 'fire_down':
+        player2Bullet["pos"] = [pos_player2[0], pos_player2[1]]
+        player2Bullet["way"] = "down"
+    elif result2 == 'fire_right':
+        player2Bullet["pos"] = [pos_player2[0], pos_player2[1]]
+        player2Bullet["way"] = "right"
+    elif result2 == 'fire_left':
+        player2Bullet["pos"] = [pos_player2[0], pos_player2[1]]
+        player2Bullet["way"] = "left"
+def Bullet_up(player1Bullet, player2Bullet, pos_player1, pos_player2, game_map, player1Xp, player2Xp):
     if player1Bullet["way"] == "left":
-        player1Bullet["pos"][1] = player1Bullet["pos"][1] - 1
-    if player1Bullet["way"] == "right":
-        player1Bullet["pos"][1] = player1Bullet["pos"][1] + 1
-    if player1Bullet["way"] == "up":
-        player1Bullet["pos"][0] = player1Bullet["pos"][0] - 1
-    if player1Bullet["way"] == "down":
-        player1Bullet["pos"][0] = player1Bullet["pos"][0] + 1
+        player1Bullet["pos"][1] -= 1
+    elif player1Bullet["way"] == "right":
+        player1Bullet["pos"][1] += 1
+    elif player1Bullet["way"] == "up":
+        player1Bullet["pos"][0] -= 1
+    elif player1Bullet["way"] == "down":
+        player1Bullet["pos"][0] += 1
+
+    if player1Bullet["pos"] == pos_player2:
+        player1Bullet["pos"] = [0, 0]
+        player1Bullet["way"] = ""
+        player2Xp -= 1
+    elif game_map[player1Bullet["pos"][0]][player1Bullet["pos"][1]] == 1:
+        player1Bullet["pos"] = [0, 0]
+        player1Bullet["way"] = ""
+    elif game_map[player1Bullet["pos"][0]][player1Bullet["pos"][1]] == 3:
+        game_map[player1Bullet["pos"][0]][player1Bullet["pos"][1]] = 0
+        player1Bullet["pos"] = [0, 0]
+        player1Bullet["way"] = ""
+
+    if player2Bullet["way"] == "left":
+        player2Bullet["pos"][1] -= 1
+    elif player2Bullet["way"] == "right":
+        player2Bullet["pos"][1] += 1
+    elif player2Bullet["way"] == "up":
+        player2Bullet["pos"][0] -= 1
+    elif player2Bullet["way"] == "down":
+        player2Bullet["pos"][0] += 1
+
+    if player2Bullet["pos"] == pos_player1:
+        player2Bullet["pos"] = [0, 0]
+        player2Bullet["way"] = ""
+        player1Xp -= 1
+    elif game_map[player2Bullet["pos"][0]][player2Bullet["pos"][1]] == 1:
+        player2Bullet["pos"] = [0, 0]
+        player2Bullet["way"] = ""
+    elif game_map[player2Bullet["pos"][0]][player2Bullet["pos"][1]] == 3:
+        game_map[player2Bullet["pos"][0]][player2Bullet["pos"][1]] = 0
+        player2Bullet["pos"] = [0, 0]
+        player2Bullet["way"] = ""
 
 def game():
     engine = sdk.GameEngineClient()
@@ -50,7 +111,7 @@ def game():
                 [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1],
                 [1, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1, 0, 0, 1],
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 3, 1],
-                [1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     engine.start()
@@ -67,8 +128,9 @@ def game():
 
     player2 = team2.players[0]
     player2.name = 2
-    pos_player2 = [0, 0]
-    coordsBul = [0, 0]
+    player2Bullet = {"pos": [0, 0], "way": ""}
+    pos_player2 = [1, 6]
+    player2Way = {"way": " "}
 
     player2Xp = 5
     player1Xp = 5
@@ -76,34 +138,183 @@ def game():
     player1Bullet = {"pos": [0, 0], "way": ""}
     integr = 0
 
-
     pos_player1 = [6, 1]
-    baseCoords = [6, 1]
+    player1Way = {"way": " "}
 
     while True:
         integr += 1
-        #result1 = sdk.timeout_run(0.4, player1.script, "make_choice", [left_history])
+        # result1 = sdk.timeout_run(0.4, player1.script, "make_choice", [left_history])
         result1 = make_choice()
+        result2 = make_choice()
         if integr == 4:
             integr = 0
-            MoveTanks(result1, game_map, pos_player1)
-            print(result1)
-        Fire(game_map, result1, pos_player1, player1Xp, player1Bullet, pos_player2, player2Xp)
-        Bullet_up(player1Bullet)
+            player1Bullet["pos"] = [0, 0]
+            MoveTanks(result1, result2, game_map, pos_player1, player1Way, pos_player2, player2Way)
+            print(player1Way["way"])
+            Fire(game_map, result1, result2, pos_player1, player1Xp, player1Bullet, pos_player2, player2Xp,
+                 player2Bullet)
 
+        Bullet_up(player1Bullet, player2Bullet, pos_player1, pos_player2, game_map, player1Xp, player2Xp)
         frame = {"field": game_map,
-                 "rotate": rotation,
                  "x": pos_player1[0],
                  "y": pos_player1[1],
-                 "baseX": baseCoords[0],
-                 "baseY": baseCoords[1],
-                 "bx": player1Bullet["pos"][0],
-                 "by": player1Bullet["pos"][1]}
+                 "x2": pos_player2[0],
+                 "y2": pos_player2[1],
+                 "bullet":
+                     {"bx": player1Bullet["pos"][0],
+                      "by": player1Bullet["pos"][1],
+                      "way": player1Bullet["way"]},
+                 "bullet2":
+                     {"bx": player2Bullet["pos"][0],
+                      "by": player2Bullet["pos"][1],
+                      "way": player2Bullet["way"]},
+                 "way1": player1Way['way'],
+                 "way2": player2Way['way']}
+
         engine.send_frame(frame)
         time.sleep(0.7)
 
     engine.end()
 
-
 if __name__ == "__main__":
     game()
+
+
+
+
+
+
+
+
+
+#########################################
+# import ge_sdk as sdk
+# import random
+# import time
+#
+# def make_choice():
+#     actions = ["up", "down",
+#                "left", "right", "fire_up", "fire_down", "fire_right", "fire_left"]
+#     # actions = ["left", "fire_right", "fire_left"]
+#     return random.choice(actions)
+#
+# def MoveTanks(result1, game_map, pos_player1, player1Way):
+#     if result1 == 'left' and game_map[pos_player1[0]][pos_player1[1] - 1] not in [1, 3]:
+#         pos_player1[1] = pos_player1[1] - 1
+#         player1Way["way"] = "left"
+#     elif result1 == 'right' and game_map[pos_player1[0]][pos_player1[1] + 1] not in [1, 3]:
+#         pos_player1[1] = pos_player1[1] + 1
+#         player1Way["way"] = "right"
+#     elif result1 == 'down' and game_map[pos_player1[0] + 1][pos_player1[1]] not in [1, 3]:
+#         pos_player1[0] = pos_player1[0] + 1
+#         player1Way["way"] = "down"
+#     elif result1 == 'up' and game_map[pos_player1[0] - 1][pos_player1[1]] not in [1, 3]:
+#         pos_player1[0] = pos_player1[0] - 1
+#         player1Way["way"] = "up"
+#
+# def Fire(game_map, result1, pos_player1, player1Xp, player1Bullet, pos_player2, player2Xp, player1Way):
+#     if result1 == 'fire_up':
+#         player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
+#         player1Bullet["way"] = "up"
+#         player1Way['way'] = "up"
+#     elif result1 == 'fire_down':
+#         player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
+#         player1Bullet["way"] = "down"
+#         player1Way['way'] = "down"
+#     elif result1 == 'fire_right':
+#         player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
+#         player1Bullet["way"] = "right"
+#         player1Way['way'] = "right"
+#     elif result1 == 'fire_left':
+#         player1Bullet["pos"] = [pos_player1[0], pos_player1[1]]
+#         player1Bullet["way"] = "left"
+#         player1Way['way'] = "left"
+#
+# def Bullet_up(player1Bullet, pos_player2, game_map, player2Xp):
+#     if player1Bullet["way"] == "left":
+#         player1Bullet["pos"][1] -= 1
+#     elif player1Bullet["way"] == "right":
+#         player1Bullet["pos"][1] += 1
+#     elif player1Bullet["way"] == "up":
+#         player1Bullet["pos"][0] -= 1
+#     elif player1Bullet["way"] == "down":
+#         player1Bullet["pos"][0] += 1
+#
+#     if player1Bullet["pos"] == pos_player2:
+#         player1Bullet["pos"] = [0, 0]
+#         player1Bullet["way"] = ""
+#         player2Xp -= 1
+#     elif game_map[player1Bullet["pos"][0]][player1Bullet["pos"][1]] == 1:
+#         player1Bullet["pos"] = [0, 0]
+#         player1Bullet["way"] = ""
+#     elif game_map[player1Bullet["pos"][0]][player1Bullet["pos"][1]] == 3:
+#         game_map[player1Bullet["pos"][0]][player1Bullet["pos"][1]] = 0
+#         player1Bullet["pos"] = [0, 0]
+#         player1Bullet["way"] = ""
+#
+# def game():
+#     engine = sdk.GameEngineClient()
+#
+#     game_map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1],
+#                 [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 3, 0, 1],
+#                 [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1],
+#                 [1, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1, 0, 0, 1],
+#                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 3, 1],
+#                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+#                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+#
+#     engine.start()
+#
+#     team1 = engine.teams[0]
+#     team1.name = 'first'
+#     left_history = []
+#     rotation = 0
+#     player1 = team1.players[0]
+#     player1.name = 1
+#
+#     team2 = engine.teams[1]
+#     team2.name = 'second'
+#
+#     player2 = team2.players[0]
+#     player2.name = 2
+#     pos_player2 = [0, 0]
+#
+#     player2Xp = 5
+#     player1Xp = 5
+#
+#     player1Bullet = {"pos": [0, 0], "way": ""}
+#     integr = 0
+#
+#     pos_player1 = [6, 2]
+#     player1Way = {"way": "up"}
+#
+#     while True:
+#         integr += 1
+#         #result1 = sdk.timeout_run(0.4, player1.script, "make_choice", [left_history])
+#         result1 = make_choice()
+#         if integr == 4:
+#             integr = 0
+#             player1Bullet["pos"] = [0, 0]
+#             MoveTanks(result1, game_map, pos_player1, player1Way)
+#             print(result1)
+#             Fire(game_map, result1, pos_player1, player1Xp, player1Bullet, pos_player2, player2Xp, player1Way)
+#
+#         Bullet_up(player1Bullet, pos_player1, game_map, player2Xp)
+#         frame = {"field": game_map,
+#                  "x": pos_player1[0],
+#                  "y": pos_player1[1],
+#                  "bullet":
+#                      {"bx": player1Bullet["pos"][0],
+#                       "by": player1Bullet["pos"][1],
+#                       "way": player1Bullet["way"]},
+#                  "ways": player1Way['way']}
+#
+#         engine.send_frame(frame)
+#         time.sleep(0.7)
+#
+#     engine.end()
+#
+#
+# if __name__ == "__main__":
+#     game()
--- games/tanks_pvp/frontend/static/script.js
+++ games/tanks_pvp/frontend/static/script.js
@@ -9,9 +9,23 @@ const tankIma = new Image();
 const tankImaRight = new Image();
 const tankImaDown = new Image();
 const tankImaLeft = new Image();
+
+const tankImaRedR = new Image();
+const tankImaRedL = new Image();
+const tankImaRedU = new Image();
+const tankImaRedD = new Image();
+
 const fil3 = new Image();
-const fil4 = new Image();
+
 const bul = new Image();
+const bulL = new Image();
+const bulR = new Image();
+const bulD = new Image();
+
+const bulRedU = new Image();
+const bulRedL = new Image();
+const bulRedR = new Image();
+const bulRedD = new Image();
 
 
 function load_images() {
@@ -22,16 +36,29 @@ function load_images() {
         tankImaRight.src = path + "/images/tankRight.png"
         tankImaDown.src = path + "/images/tankDown.png"
         tankImaLeft.src = path + "/images/tankLeft.png"
+
+        tankImaRedR.src = path + "/images/tanksRedR.png"
+        tankImaRedD.src = path + "/images/tanksRedD.png"
+        tankImaRedL.src = path + "/images/tanksRedLeft.png"
+        tankImaRedU.src = path + "/images/tanksRedU.png"
+
         fil.src = path + "/images/field.png";
         fil2.src = path + "/images/wall2.jpeg";
         fil3.src = path + "/images/brokenWall.jpeg";
-        fil4.src = path + "/images/field2.png";
         bul.src = path + "/images/bulBkue.png";
+        bulL.src = path + "/images/bullL.png";
+        bulR.src = path + "/images/bullR.png";
+        bulD.src = path + "/images/bullD.png";
+
+        bulRedU.src = path + "/images/bulRedU.png";
+        bulRedL.src = path + "/images/bulRedL.png";
+        bulRedR.src = path + "/images/bulRedR.png";
+        bulRedD.src = path + "/images/bulRedD.png";
 
         const image_loaded = () => {
             loaded++;
 
-            if (loaded === 9) {
+            if (loaded === 19) {
                 resolve();
             }
         }
@@ -42,35 +69,60 @@ function load_images() {
         tankImaRight.onload = image_loaded
         tankImaDown.onload = image_loaded
         tankImaLeft.onload = image_loaded
+
+        tankImaRedR.onload = image_loaded
+        tankImaRedU.onload = image_loaded
+        tankImaRedL.onload = image_loaded
+        tankImaRedD.onload = image_loaded
+
         fil3.onload = image_loaded
-        fil4.onload = image_loaded
         bul.onload = image_loaded
+        bulL.onload = image_loaded
+        bulD.onload = image_loaded
+        bulR.onload = image_loaded
+
+        bulRedU.onload = image_loaded
+        bulRedD.onload = image_loaded
+        bulRedL.onload = image_loaded
+        bulRedR.onload = image_loaded
     })
 }
 
-function R(img, degrees, x, y, s){
-    ctx.translate(x,y);
-    ctx.rotate(-degrees + Math.PI/2.0);
-    ctx.drawImage(img,x,y,s,s);
-    ctx.rotate(degrees - Math.PI/2.0);
-    ctx.translate(-x, -y);
-}
-
-function drawField(ctx, field, rotation, tx, ty, bbx, bby, bx, by) {
+function drawField(ctx, field, tx, ty, ttx, tty, bx, by, way, bbx, bby, bWay, tWay, ttWay) {
     const cellSize = 85.3;
     for (let i = 0; i < 15; i++) {
         for (let j = 0; j < 8; j++) {
             const x = -10 + cellSize * i
             const y = -50 + cellSize * j
-            if (field[j][i] === 0){ctx.drawImage(fil, x, y, cellSize, cellSize)}
-            else if (field[j][i] === 1){ctx.drawImage(fil2, x, y, cellSize, cellSize)}
-            else if(field[j][i] === 3){ctx.drawImage(fil3, x, y, cellSize, cellSize)}
-            else if(field[j][i] === 6){ctx.drawImage(fil4, -10 + cellSize * bby, -50 + cellSize * bbx, cellSize, cellSize)}
+            if (field[j][i] === 0) { ctx.drawImage(fil, x, y, cellSize, cellSize) }
+            else if (field[j][i] === 1) { ctx.drawImage(fil2, x, y, cellSize, cellSize) }
+            else if (field[j][i] === 3) { ctx.drawImage(fil3, x, y, cellSize, cellSize) }
+            //else if(field[j][i] === 6){ctx.drawImage(fil4, -10 + cellSize * bby, -50 + cellSize * bbx, cellSize, cellSize)}
         }
     }
-    ctx.drawImage(tankIma, -10 + ty * cellSize, -50 + tx * cellSize, cellSize, cellSize);
-    if (bx != 0 && by!= 0){ctx.drawImage(bul, -10 + by * cellSize, -50 + bx * cellSize, cellSize, cellSize);}
+    if (tWay === "left") { ctx.drawImage(tankImaLeft, -10 + ty * cellSize, -50 + tx * cellSize, cellSize, cellSize); }
+    else if (tWay === "down") { ctx.drawImage(tankImaDown, -10 + ty * cellSize, -50 + tx * cellSize, cellSize, cellSize); }
+    else if (tWay === "up") { ctx.drawImage(tankIma, -10 + ty * cellSize, -50 + tx * cellSize, cellSize, cellSize); }
+    else if (tWay === "right") { ctx.drawImage(tankImaRight, -10 + ty * cellSize, -50 + tx * cellSize, cellSize, cellSize); }
 
+    if (bx != 0 && by != 0 && tWay == way) {
+        if (way === "left") { ctx.drawImage(bulL, -10 + by * cellSize, -50 + bx * cellSize, cellSize, cellSize); }
+        else if (way === "down") { ctx.drawImage(bulD, -10 + by * cellSize, -50 + bx * cellSize, cellSize, cellSize); }
+        else if (way === "up") { ctx.drawImage(bul, -10 + by * cellSize, -50 + bx * cellSize, cellSize, cellSize); }
+        else if (way === "right") { ctx.drawImage(bulR, -10 + by * cellSize, -50 + bx * cellSize, cellSize, cellSize); }
+    }
+
+    if (ttWay === "left") { ctx.drawImage(tankImaRedL, -10 + tty * cellSize, -50 + ttx * cellSize, cellSize, cellSize); }
+    else if (ttWay === "down") { ctx.drawImage(tankImaRedD, -10 + tty * cellSize, -50 + ttx * cellSize, cellSize, cellSize); }
+    else if (ttWay === "up") { ctx.drawImage(tankImaRedU, -10 + tty * cellSize, -50 + ttx * cellSize, cellSize, cellSize); }
+    else if (ttWay === "right") {ctx.drawImage(tankImaRedR, -10 + tty * cellSize, -50 + ttx * cellSize, cellSize, cellSize);}
+
+    if (bx != 0 && by != 0 && ttWay == bWay) {
+        if (bWay === "left") { ctx.drawImage(bulRedL, -10 + bby * cellSize, -50 + bbx * cellSize, cellSize, cellSize); }
+        else if (bWay === "down") { ctx.drawImage(bulRedD, -10 + bby * cellSize, -50 + bbx * cellSize, cellSize, cellSize); }
+        else if (bWay === "up") { ctx.drawImage(bulRedU, -10 + bby * cellSize, -50 + bbx * cellSize, cellSize, cellSize); }
+        else if (bWay === "right") { ctx.drawImage(bulRedR, -10 + bby * cellSize, -50 + bbx * cellSize, cellSize, cellSize); }
+    }
 
 }
 
@@ -83,7 +135,9 @@ function init() {
 
 function newFrame(frame) {
     const ctx = document.getElementById("field").getContext("2d");
-    drawField(ctx, frame["field"], frame["rotate"], frame['x'], frame['y'], frame['baseX'], frame['baseY'], frame['bx'], frame['by'])
+    drawField(ctx, frame["field"], frame['x'], frame['y'], frame['x2'], frame['y2'],
+        frame["bullet"]['bx'], frame["bullet"]['by'], frame["bullet"]["way"], frame["bullet2"]['bx'],
+        frame["bullet2"]['by'], frame["bullet2"]["way"], frame['way1'], frame['way2'])
 
     console.log(frame)
 }
